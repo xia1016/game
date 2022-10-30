c8a7465f03ad4c1208ed21955782f12430137ad6
xiahongfei <Xiababa_1016@163.com>
2022-10-30 20:23:10 +0800

    feat: init

---
+++ components/Modal/modal.js
@@ -0,0 +1,37 @@
+// components/modals.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        isSuccess:false,
+        isFail:false
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        success(){
+            this.setData({
+                isSuccess:true
+            })
+        },
+        fail(){
+            this.setData({
+                isFail:true
+            })
+        },
+        reload() {
+            console.log(222);
+            this.triggerEvent('initImgList')
+        }
+    },
+})
